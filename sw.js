26d837e82a771cc3a965489f43875f56c84c8e93
shawnyyyx <shawnyyyx@users.noreply.github.com>
2026-07-30 09:46:29 +0800

    修复Service Worker缓存导致页面不更新

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
-const CACHE = 'morning-brief-v1';
-const ASSETS = ['/daily-briefing/', '/daily-briefing/index.html', '/daily-briefing/data.json'];
+const CACHE = 'morning-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('/daily-briefing/data.json')) {
-    e.respondWith(
-      fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
-  }
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
